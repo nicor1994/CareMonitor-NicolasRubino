09f5b8a1e4914eb9de9009e5a1bbd749dd66ed4d
PC Quirofano <nicolasjrubino@gmail.com>
2020-09-04 20:54:13 -0300

    04/09/2020

---
+++ GUI/js/Local/Validacion.js
@@ -0,0 +1,18 @@
+﻿function ValidarStringVacio(elementId) {
+
+    var Valor = document.getElementById(elementId).value;
+    if (!Valor || 0 === value.length || /^\s+$/.test(Valor)) {
+        return false; //El String esta vacio
+    } else {
+        return true; //El String NO esta vacio
+    }
+
+}
+
+function ValidarStringSinNumeros{
+
+    var Valor = document.getElementById(elementId).value
+    if (isNaN(Valor)) {
+        return 
+    }
+}
